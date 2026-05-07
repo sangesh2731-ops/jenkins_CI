af0923b70491f54eca4c13ac0b90f67811f6d7d8
sangesh2731-ops <sangesh2731@gmail.com>
2026-05-06 22:18:00 -0700

    Create App.java

---
+++ App.java
@@ -0,0 +1,9 @@
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello, Jenkins CI!");
+    }
+
+    public String getGreeting() {
+        return "Hello World";
+    }
+}
